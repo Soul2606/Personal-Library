1a753571232f08670c30da836d02aba194490f57
Sondre Ulvik <sondulv@gmail.com>
2026-05-21 19:29:55 +0200

    - added json related functions

---
+++ src/json/utils.ts
@@ -0,0 +1,80 @@
+import type { JSONValue } from "../types"
+
+type WalkJsonArgs = { 
+	key: string|number|null
+	value: JSONValue 
+	parent: null|Record<string,JSONValue>|JSONValue[]
+	path: readonly (string | number)[] 
+	isLeaf: false
+	mutateValue: (newValue: JSONValue)=>void
+	mutateKey: (newKey: string|number)=>void
+} | { 
+	key: string|number|null
+	value: string|number|null|boolean
+	parent: null|Record<string,JSONValue>|JSONValue[]
+	path: readonly (string | number)[] 
+	isLeaf: true
+	mutateValue: (newValue: JSONValue)=>void
+	mutateKey: (newKey: string|number)=>void
+}
+
+export function walkJson(obj: JSONValue, fnc: (args:WalkJsonArgs)=>void): void {
+	type Parental = null|
+	{readonly kind:"object", readonly key: string, readonly value: Record<string, JSONValue>}|
+	{readonly kind:"array", readonly key: number, readonly value: JSONValue[]}
+
+	const recurse = (
+		current:JSONValue, 
+		parent: Parental = null, 
+		path:   Array<string | number> = []
+	) => {
+		const mutateValue = (newValue: JSONValue)=>{
+			if (parent === null) return false
+			if (parent.kind === "object"){
+				parent.value[parent.key] = newValue
+			} else {
+				parent.value[parent.key] = newValue
+			}
+			return true
+		}
+
+		const mutateKey = (newKey: string|number)=>{
+			if (parent === null) return false
+			if (parent.kind === "object") {
+				if (typeof newKey === "number") return false
+				delete parent.value[parent.key]
+			}
+			if (parent.kind === "array") {
+				if (typeof newKey === "string") return false
+				parent.value.splice(parent.key, 1)
+			}
+			//@ts-ignore
+			parent.value[newKey] = current
+			return true
+		}
+
+		const isLeaf = current === null || typeof current === "number" || typeof current === "boolean" || typeof current === "string"
+		fnc({
+			key: parent ? parent.key : null,
+			value: current as any,
+			parent: parent ? parent.value : null,
+			path,
+			isLeaf,
+			mutateValue,
+			mutateKey
+		})
+
+		// Recurse into children if object/array
+		if (Array.isArray(current)) {
+			current.forEach((val, idx) => recurse(val, {value:current, key:idx, kind:"array"}, [...path, idx]))
+		} else if (typeof current === 'object') {
+			for (const key in current) {
+				const next = current[key]
+				if (next === undefined) continue
+				recurse(next, {value:current, key, kind:"object"}, [...path, key])
+			}
+		}
+	}
+
+	return recurse(obj, null, [])
+}
